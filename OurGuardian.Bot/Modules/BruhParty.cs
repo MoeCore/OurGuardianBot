fe59731db0f26570b4e934df0e3caf66effbe9b6
Ray <60722967+MoeCore@users.noreply.github.com>
2022-09-19 08:18:27 +0400

    Added bruh party for fun

---
+++ OurGuardian.Bot/Modules/BruhParty.cs
@@ -0,0 +1,30 @@
+using Discord;
+using Discord.Interactions;
+using Microsoft.Extensions.Logging;
+
+namespace OurGuardian.Bot.Modules;
+
+public class BruhParty : InteractionModuleBase<SocketInteractionContext>
+{
+    public readonly ILogger _logger;
+
+    public BruhParty(ILogger<BruhParty> logger)
+    {
+        _logger = logger;
+    }
+
+    [SlashCommand("Bruh", "Change all nicknames to Bruh")]
+    // [DefaultMemberPermissions(GuildPermission.ManageNicknames)]
+    public async Task Bruh()
+    {
+        _logger.LogTrace("User {Username}#{UserTag} set bruh party!", Context.User.Username, Context.User.Discriminator);
+
+        foreach (var user in Context.Guild.Users)
+        {
+            await user.ModifyAsync((mod) =>
+            {
+                mod.Nickname = "Bruh";
+            });
+        }
+    }
+}
