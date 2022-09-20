e9fe151221d51ebb8fb219e125c9fd3760d28056
Ray <60722967+MoeCore@users.noreply.github.com>
2022-09-20 20:10:26 +0400

    Moved from yaml to json

--- OurGuardian.Bot/Program.cs
+++ OurGuardian.Bot/Program.cs
@@ -1,58 +1,63 @@
-﻿using Discord.Commands;
+﻿using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Yaml;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using OurGuardian.Bot.Services;
 using Serilog;
 
 internal class Program
 {
-    private static async Task Main()
+    private readonly IServiceProvider _services;
+    private readonly DiscordSocketConfig _socketConfig;
+    private readonly IConfiguration _configuration;
+
+    public Program()
     {
-        IConfigurationRoot config = new ConfigurationBuilder()
+        _configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddYamlFile("Config.yml")
+            .AddJsonFile("appsettings.json", optional: false)
             .Build();
 
-        using IHost host = Host.CreateDefaultBuilder()
-            .ConfigureServices((_, services) => services
-                .AddSingleton(config)
-                .AddSingleton<DiscordSocketClient>()
-                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
-                .AddSingleton(x => new CommandService(new CommandServiceConfig
-                {
-                    LogLevel = Discord.LogSeverity.Debug,
-                    DefaultRunMode = Discord.Commands.RunMode.Async
-                }))
-                .AddSingleton<OurGuardian.Bot.Services.Interaction>()
-                .AddSingleton<OurGuardian.Bot.Services.Log>())
-                .UseSerilog((context, services, configuration) => configuration
-                    .ReadFrom.Configuration(context.Configuration)
-                    .ReadFrom.Services(services)
-                    .Enrich.FromLogContext()
-                    .WriteTo.Console()
-                    .MinimumLevel.Verbose())
-                .Build();
+        _socketConfig = new DiscordSocketConfig()
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers,
+            AlwaysDownloadUsers = true,
+        };
 
-        await RunAsync(host);
+        _services = new ServiceCollection()
+            .AddSingleton(_configuration)
+            .AddSingleton(_socketConfig)
+            .AddSingleton<DiscordSocketClient>()
+            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+            .AddSingleton<InteractionHandler>()
+            .AddSingleton<DiscordSocketClientHandler>()
+            .AddLogging(builder => builder.AddSerilog(dispose: true))
+            .BuildServiceProvider();
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.File($"Logs/{DateTime.Now:yyyy-MM-dd}/{DateTime.Now:HH-mm-ss}.txt")
+            .WriteTo.Console()
+            .CreateLogger();
     }
 
-    private static async Task RunAsync(IHost host)
+    private static void Main()
+        => new Program().RunAsync().GetAwaiter().GetResult();
+
+    private async Task RunAsync()
     {
-        using IServiceScope serviceScope = host.Services.CreateScope();
-        IServiceProvider provider = serviceScope.ServiceProvider;
+        await _services.GetRequiredService<InteractionHandler>().InitializeAsync();
+        await _services.GetRequiredService<DiscordSocketClientHandler>().InitializeAsync();
+        await Task.Delay(Timeout.Infinite);
+    }
 
-        await provider.GetRequiredService<OurGuardian.Bot.Services.Log>().Init();
-        await provider.GetRequiredService<OurGuardian.Bot.Services.Interaction>().Init();
-
-        var client = provider.GetRequiredService<DiscordSocketClient>();
-        var config = provider.GetRequiredService<IConfigurationRoot>();
-
-        await client.LoginAsync(Discord.TokenType.Bot, config["Tokens:Discord"]);
-        await client.StartAsync();
-
-        await Task.Delay(-1);
+    public static bool IsDebug()
+    {
+#if DEBUG
+        return true;
+#else
+        return false;
+#endif
     }
 }
